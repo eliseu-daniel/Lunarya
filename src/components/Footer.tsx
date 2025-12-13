1f4a70eca8b53f27775f26347b5b439feb6e2a8d
eliseu <eliseudaniel29@gmail.com>
2025-12-12 23:51:08 -0300

    feat: footer component update policy and terms

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <p>&copy; 2024 @Lunarya. Todos os direitos reservados.</p>
+            <a href=""> Politica de Privacidade</a>
+            <a href=""> Termos de Serviço</a>
         </footer>
     );
 }
